6a2829d2e7ba018320afa746963f2eb90b5fe2f3
will <williamwalker753@gmail.com>
2022-09-08 03:02:14 +0000

    fix nginx headers

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@ startPath=$(pwd)
 scriptPath=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 
 cd $scriptPath
+# docker container stop trying-out-3d_app_1
+# docker container rm trying-out-3d_app_1
+# docker rmi nginx:1.23.1-alpine
 docker-compose up
 cd $startPath
